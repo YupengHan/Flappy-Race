725250a94fd7877e268813017dda3674278dc52d
Yupeng Han <38163934+YupengHan@users.noreply.github.com>
2018-04-22 14:42:21 -0400

    sql mid exist fixed

--- Flappy-Race/app.js
+++ Flappy-Race/app.js
@@ -58,22 +58,74 @@ function onConnection(sock) {
         io.in(Object.keys(sock.rooms)[0]).emit('firec', x, y, direction);
     });
     sock.on('login', function (nameAndPass) {
+
+
+
         console.log(nameAndPass);
+
         var midexist;
+        var wincount;
         con.connect(function(err) {
         	if (err) throw err;
         	console.log("Connected!");
-        	var sql = "SELECT EXISTS(SELECT 1 FROM customer WHERE name='a');";
+        	var sql = "SELECT EXISTS(SELECT 1 FROM users WHERE nameandpass='"+nameAndPass+"');";
+        	console.log(sql);        		
         	con.query(sql, function (err, result) {
+        		var result_str=JSON.stringify(result);
+        		console.log(result_str);
+        		console.log("result_str.indexOf(})"+result_str.indexOf("}"));
+        		var result_index_num=result_str.indexOf("}")-1;
+        		console.log('result_index_num'+result_index_num);
+        		midexist=result_str[result_index_num];
+        		console.log(midexist);
+
         		if (err) throw err;
-        		console.log("1 record inserted");
+        		// console.log(result);
+        		// console.log("Object.keys(result)[1]:"+Object.keys(result)[1]);
+        		// console.log("Object.keys(result): "+Object.keys(result));
+        		// console.log("Object.getOwnPropertyNames(result)[0]: "+Object.getOwnPropertyNames(result[0])   );	
+        		// console.log('------------------------------------------------------')
+        		// console.log('result[0]'+result[0][0]);
+        		// console.log('result[1]'+result[0][1]);
+        		// var prop_name=Object.getOwnPropertyNames(result)[0];
+        		// console.log(prop_name);
+        		// console.log(result.prop_name);
+        		// console.log('prop_name:'+prop_name);
+        		// const desp1 = Object.getOwnPropertyDescriptor(result,prop_name);
+        		// midexis=desp1.value;
+
+        		// console.log(midexist);
+        		console.log("Exisits");
         		midexist=true;
         	});
+
         });
 
         if(midexist){
-            sock.emit('login', nameAndPass, users.get(nameAndPass));
+        	console.log("midexist=true");
+        	var wincount;
+        	// Hard Code
+        	con.connect(function(err) {
+        	if (err) throw err;
+        	console.log("Connected!");
+        	var sql = "SELECT wincount from users where nameandpass ='"+nameAndPass+"';";
+        	console.log(sql);
+        	con.query(sql, function (err, result) {
+        		console.log('-----------------------------------------------------------------------')
+        		console.log(result);
+        		console.log(rows[0]['wincount']);
+        		console.log('-----------------------------------------------------------------------')
+        		if (err) throw err;
+        		console.log("Wincount is :"+result);
+        		midexist=true;
+        	});
+        	});
+
+
+        	// wincount=0;
+            sock.emit('login', nameAndPass, wincount);
         } else {
+            console.log('Guseting')
             sock.emit('login', 'guest');
         }
     });
